e9c6c7ae3e880cd445f9893814568e309d22868c
sbower <shawn.bower@gmail.com>
2015-06-23 22:17:33 -0400

    fixing up sync to allow for directional choice

--- sync.rb
+++ sync.rb
@@ -5,16 +5,24 @@ unless ENV['S3_BUCKET']
   exit 1
 end
 
+DIRECTION = ENV['DIRECTION'] || 'BI'
+SLEEP_SECONDS = ENV['SLEEP_SECONDS'] || 1800
+
 def get_ts
   Time.now.utc.to_s
 end
 
 loop do
   start = Time.now
-  puts "#{get_ts} starting sync"
-  puts `aws s3 sync /sync s3://#{ENV['S3_BUCKET']}`
-  puts "#{get_ts} finished upload, took #{Time.now - start}"
-  puts `aws s3 sync s3://#{ENV['S3_BUCKET']} /sync`
-  puts "#{get_ts} finished sync, took #{Time.now - start}"
-  sleep 30
+  
+  if DIRECTION.eql?("BI") or DIRECTION.eql?("UP")
+    puts "#{get_ts} starting sync"
+    puts `aws s3 sync /sync s3://#{ENV['S3_BUCKET']}`
+    puts "#{get_ts} finished upload, took #{Time.now - start}"
+  elsif DIRECTION.eql?("BI") or DIRECTION.eql?("DN")
+    puts `aws s3 sync s3://#{ENV['S3_BUCKET']} /sync`
+    puts "#{get_ts} finished sync, took #{Time.now - start}"
+  end
+  
+  sleep SLEEP_SECONDS
 end
